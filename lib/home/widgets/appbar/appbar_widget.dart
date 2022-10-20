399ed29fd3cb3fc940f4a670d00d65a704678439
Renato Ruppel <heruppel2@gmail.com>
2022-10-20 15:41:40 -0300

    feat(scorecard): Included score card widget at appbar

--- lib/home/widgets/appbar/appbar_widget.dart
+++ lib/home/widgets/appbar/appbar_widget.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:nlw5/core/app_gradients.dart';
 import 'package:nlw5/core/app_text_styles.dart';
 
+import '../score_card/score_card_widget.dart';
+
 class AppBarWidget extends PreferredSize {
   AppBarWidget()
       : super(
@@ -13,26 +15,33 @@ class AppBarWidget extends PreferredSize {
                 ),
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text.rich(TextSpan(
-                            text: 'Olá',
-                            style: AppTextStyles.title,
-                            children: [
-                              TextSpan(
-                                  text: 'Renato',
-                                  style: AppTextStyles.titleBold)
-                            ])),
-                        Container(
-                          width: 58,
-                          height: 58,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              image: DecorationImage(
-                                  image: NetworkImage(
-                                      'https://avatars.githubusercontent.com/u/61123552?v=4'))),
-                        )
-                      ]),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text.rich(TextSpan(
+                              text: 'Olá',
+                              style: AppTextStyles.title,
+                              children: [
+                                TextSpan(
+                                    text: 'Renato',
+                                    style: AppTextStyles.titleBold)
+                              ])),
+                          Container(
+                            width: 58,
+                            height: 58,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                image: DecorationImage(
+                                    image: NetworkImage(
+                                        'https://avatars.githubusercontent.com/u/61123552?v=4'))),
+                          )
+                        ],
+                      ),
+                      ScoreCardWidget()
+                    ],
+                  ),
                 )));
 }
