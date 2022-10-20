2732d39927f2f89aa06b58b9e67415fada4efd69
Renato Ruppel <heruppel2@gmail.com>
2022-10-20 15:30:44 -0300

    Init

---
+++ lib/core/app_widget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:nlw5/home/home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'DevQuiz', home: HomePage());
+  }
+}
