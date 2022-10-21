c4a8e20fc83143a8b54478f13fb2f1dc8b40c6f2
Renato Ruppel <heruppel2@gmail.com>
2022-10-21 12:28:45 -0300

    feat(button): created level button

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nlw5/home/widgets/appbar/appbar_widget.dart';
+import 'package:nlw5/home/widgets/level_button/level_button_widget.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -12,7 +13,18 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarWidget(),
-    );
+        appBar: AppBarWidget(),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              LevelButtonWidget(label: 'Fácil'),
+              LevelButtonWidget(label: 'Médio'),
+              LevelButtonWidget(label: 'Difícil'),
+              LevelButtonWidget(label: 'Perito'),
+            ],
+          ),
+        ));
   }
 }
