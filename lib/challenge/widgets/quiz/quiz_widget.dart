ba806be17cbaad54d5a45cff8ec5b8750aef3a72
Renato Ruppel <heruppel2@gmail.com>
2022-11-19 13:10:05 -0300

    refactor(answers): reworked answers prop and added tap handling

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:nlw5/challenge/widgets/answer/answer_widget.dart';
 import 'package:nlw5/core/app_text_styles.dart';
+import 'package:nlw5/shared/models/answer_model.dart';
 import 'package:nlw5/shared/models/question_model.dart';
 
-class QuizWidget extends StatelessWidget {
+class QuizWidget extends StatefulWidget {
   final QuestionModel question;
   const QuizWidget({Key? key, required this.question}) : super(key: key);
+
+  @override
+  State<QuizWidget> createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+
+  AwnserModel answer(int index) => widget.question.awnsers[index];
 
   @override
   Widget build(BuildContext context) {
@@ -13,16 +23,16 @@ class QuizWidget extends StatelessWidget {
         child: Column(
       children: [
         SizedBox(height: 64),
-        Text(question.title, style: AppTextStyles.heading),
+        Text(widget.question.title, style: AppTextStyles.heading),
         SizedBox(height: 24),
-        ...question.awnsers
-            .map(
-              (answer) => AnswerWidget(
-                  isSelected: false,
-                  isRight: answer.isRight,
-                  title: answer.title),
-            )
-            .toList(),
+        for (var i = 0; i < widget.question.awnsers.length; i++)
+          AnswerWidget(
+              isSelected: indexSelected == i,
+              answer: answer(i),
+              onTap: () {
+                indexSelected = i;
+                setState(() {});
+              })
       ],
     ));
   }
