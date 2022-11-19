05eb563b12a2400ef1935c0d8385ed327fd032c1
Renato Ruppel <heruppel2@gmail.com>
2022-11-19 14:52:30 -0300

    feat(splash): added splash to home transition

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-
-import '../home/home_page.dart';
+import 'package:nlw5/splash/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'DevQuiz', home: HomePage());
+    return MaterialApp(title: 'DevQuiz', home: SplashPage());
   }
 }
